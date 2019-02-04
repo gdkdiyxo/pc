3d6f6297b1c3c762133e809eec371498248c1c85
Mark Yapp <markwyapp@gmail.com>
2019-02-03 18:38:48 -0700

    remove boilerplate files

---
+++ src/components/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LandingPage from "./landing-page";
+
+export default class App extends React.Component {
+  render() {
+    return <LandingPage />;
+  }
+}
---
+++ src/components/landing-page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./landing-page.css";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <nav role="navigation">Nav</nav>
+      <header role="banner">
+        <h1>Welcome to Deltio</h1>
+        <h3>Create and share beautiful digital postcards.</h3>
+      </header>
+
+      <section className="section1">Instruction 1</section>
+      <section>Instruction 2</section>
+      <section className="section3">Instruction 3</section>
+
+      <footer role="contentinfo">Footer</footer>
+    </div>
+  );
+}
