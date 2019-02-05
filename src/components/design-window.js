5b511ad6498e65adc7a2466d3f943b072f895aa4
Mark Yapp <markwyapp@gmail.com>
2019-02-04 17:56:42 -0700

    terser patch

--- src/components/design-window.js
+++ src/components/design-window.js
@@ -1,32 +1,47 @@
 import React from "react";
 
-import { unsplashAuth } from ".../env";
+import { unsplashAuth } from "./secrets";
 export default class DesignWindow extends React.Component {
-  loadImage() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: []
+    };
+  }
+  loadImage(e) {
+    e.preventDefault();
     const page = 1;
+    const per_page = 1;
     const query = "cats";
-    fetch(
-      `https://api.unsplash.com/search/photos?page=${page}&query=${query}`,
-      {
-        headers: {
-          "Content-Tyope": "application/JSON",
-          "Accept-Version": "v1",
-          Authorization: `${unsplashAuth}`
-        }
+    let url = `https://api.unsplash.com/search/photos?client_id=${unsplashAuth}&page=${page}&per_page=${per_page}&query=${query}`;
+    console.log(url);
+    fetch(`${url}`, {
+      headers: {
+        method: "GET",
+        "Content-Type": "application/JSON",
+        "Accept-Version": "v1",
+        Authorization: `${unsplashAuth}`
       }
-    )
-      .then(response => response.json)
-      .then(data => console.log(data));
+    })
+      .then(response => response.json())
+      .then(data => console.log(data.results[0]));
+    // console.log(data.results[0].urls.small);
+    // this.setState({ image: data.results[0].urls.small });
+    // });
   }
 
   render() {
     return (
       <section className="design-container">
         <div className="design">
-          <p>Image</p>
+          <img src={this.state.image} alt="this is the alt text" />
         </div>
         <div className="design-controls">
-          <button type="submit" className="flip-image-button">
+          <button
+            type="submit"
+            className="flip-image-button"
+            onClick={e => this.loadImage(e)}
+          >
             flip
           </button>
         </div>
