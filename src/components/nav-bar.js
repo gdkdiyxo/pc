4715976a3c30244c1c7e410acf9f716b2563d576
Mark Yapp <markwyapp@gmail.com>
2019-02-09 23:58:15 -0700

    working modular nav

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,29 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './nav-bar.css';
 
-export default class NavBar extends React.Component {
+function LoggedOutLinks() {
+  return (
+    <div>
+      <button className="nav-demo-btn">
+        <Link to="/create">Try the demo</Link>
+      </button>
+      <button className="nav-right-btn">
+        <Link to="/signup">Sign up</Link>
+      </button>
+      <button className="nav-right-btn">
+        <Link to="/login">Log in</Link>
+      </button>
+    </div>
+  );
+}
+
+function LoggedInLinks() {
+  return (
+    <div>
+      <button className="nav-right-btn">Account</button>
+      <button className="nav-right-btn">
+        <Link to="/">Log out</Link>
+      </button>
+    </div>
+  );
+}
+
+export class NavBar extends React.Component {
   render() {
-    return (
+    return this.props.currentUser ? (
       <nav role="navigation">
         <p className="nav-logo">Deltio</p>
-        <button>
-          <Link to="/create">Try the demo</Link>
-        </button>
-        <div className="nav-btns">
-          <button>
-            <Link to="/login">Log in</Link>
-          </button>
-          <button className="signup-btn">
-            <Link to="/signup">Sign up</Link>
-          </button>
-          <button className="account-btn">{/* <Link to="/login">Account</Link> */}Account</button>
-          <button className="logout-btn">
-            <Link to="/">Log out</Link>
-          </button>
-        </div>
+        <LoggedOutLinks />
+      </nav>
+    ) : (
+      <nav role="navigation">
+        <p className="nav-logo">Deltio</p>
+        <LoggedInLinks />
       </nav>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  userAuth: state.userAuth
+});
+
+export default connect(mapStateToProps)(NavBar);
