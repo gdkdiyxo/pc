f1ba40b9db7384d513f6b7888e10d7a754a91a81
Mark Yapp <markwyapp@gmail.com>
2019-02-15 01:05:11 -0700

    moved image form to modal

--- src/components/card/card-front.js
+++ src/components/card/card-front.js
@@ -2,14 +2,88 @@ import React from 'react';
 import './card-front.css';
 import { connect } from 'react-redux';
 
+import { setImage } from '../../actions/card';
+import '../create-page/image-form.css';
+
+export function ImageForm(props) {
+  return (
+    <div className="image-form-wrapper">
+      <form onSubmit={e => props.onSubmit(e)}>
+        <label htmlFor="search">1) Search for an image</label>
+        <div className="image-form-row">
+          <input
+            id="search"
+            placeholder="search for anything"
+            ref={input => (this.searchInput = input)}
+          />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 export class CardFront extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalShowing: false
+    };
+  }
+
+  searchImage(e) {
+    e.preventDefault();
+    const page = 1;
+    const per_page = 1;
+    const query = this.searchInput.value;
+    const unsplashAuth =
+      'Client-ID 72f712e5e78353fa3a7bb238edf115fdb80e04120f85d42b48f85ffb5e849cca';
+    let url = `https://api.unsplash.com/search/photos?page=${page}&per_page=${per_page}&query=${query}`;
+    fetch(`${url}`, {
+      headers: {
+        method: 'GET',
+        'Content-Type': 'application/JSON',
+        'Accept-Version': 'v1',
+        Authorization: `${unsplashAuth}`
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        const image = { full: data.results[0].urls.regular, alt: 'this is the alt' };
+        this.props.dispatch(setImage(image));
+      });
+  }
+
+  showImageModal(e) {
+    e.stopPropagation();
+    console.log('showImageModal clicked');
+    this.setState({
+      isModalShowing: !this.state.isModalShowing
+    });
+  }
+
   render() {
     return (
       <div className="card-image-container">
         <p className="card-flip-instruction">Click image to flip</p>
-
+        <button className="add-image-btn" onClick={e => this.showImageModal(e)}>
+          Add Image
+        </button>
         <img src={this.props.image.full} alt={this.props.image.alt} />
-
+        {/* <ImageForm isOpen={false} onSubmit={value => this.searchImage(value)} /> */}
+        <div className="image-form-wrapper" id={!this.state.isModalShowing ? 'hidden' : ''}>
+          <form onSubmit={e => this.onSubmit(e)}>
+            <label htmlFor="search">1) Search for an image</label>
+            <div className="image-form-row">
+              <input
+                id="search"
+                placeholder="search for anything"
+                ref={input => (this.searchInput = input)}
+              />
+              <button type="submit">Search</button>
+            </div>
+          </form>
+        </div>
         {/* <button type="submit" className="last-image-btn" onClick={e => this.loadNextImage(e)}>
           <i className="fas fa-7x fa-angle-left" />
         </button>
--- src/components/create-page/create-page.js
+++ src/components/create-page/create-page.js
@@ -29,7 +29,7 @@ export class CreatePage extends React.Component {
   render() {
     return (
       <div>
-        <ImageForm />
+        {/* <ImageForm /> */}
         <CardContainer />
         <MessageForm />
         <RecipientForm />
