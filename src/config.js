2deac8df66be7916a543f362642d06090caefa43
Mark Yapp <markwyapp@gmail.com>
2019-02-23 00:21:40 -0700

    fixed client base url path in config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
-export const CLIENT_BASE_URL = process.env.CLIENT_BASE_URL || 'http://localhost:3000';
+export const CLIENT_BASE_URL = process.env.REACT_APP_CLIENT_BASE_URL || 'http://localhost:3000';
 export const UNSPLASH_AUTH =
   'Client-ID 72f712e5e78353fa3a7bb238edf115fdb80e04120f85d42b48f85ffb5e849cca';
 export const DEMO_AUTH_TOKEN =
