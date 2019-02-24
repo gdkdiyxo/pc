ba37f9bf41100cec90ab39445d3632bd6e01c86c
Mark Yapp <markwyapp@gmail.com>
2019-02-24 13:52:18 -0700

    reducer tests

--- src/reducers/auth-reducer.test.js
+++ src/reducers/auth-reducer.test.js
@@ -3,6 +3,7 @@ import { setUser, fetchSuccess, fetchRequest, setAuthError } from '../actions/au
 
 describe('authReducer', () => {
   const username = 'testuser';
+  const error = 'failed to fetch';
 
   it('should set the initial state', () => {
     const state = authReducer(undefined, { type: '__UNKNOWN' });
@@ -10,6 +11,38 @@ describe('authReducer', () => {
       currentUser: null,
       loading: false,
       error: null
+    });
+  });
+
+  describe('setUser', () => {
+    it('should update state with the current user', () => {
+      let state;
+      state = authReducer(state, setUser(username));
+      expect(state.currentUser).toEqual(username);
+    });
+  });
+
+  describe('fetchRequest', () => {
+    it('should update the state loading status', () => {
+      let state;
+      state = authReducer(state, fetchRequest());
+      expect(state.loading).toEqual(true);
+    });
+  });
+
+  describe('fetchSuccess', () => {
+    it('should update the state loading status', () => {
+      let state;
+      state = authReducer(state, fetchSuccess());
+      expect(state.loading).toEqual(false);
+    });
+  });
+
+  describe('setAuthError', () => {
+    it('should set the state error to current error', () => {
+      let state;
+      state = authReducer(state, setAuthError(error));
+      expect(state.error).toEqual(error);
     });
   });
 });
--- src/reducers/card-reducer.test.js
+++ src/reducers/card-reducer.test.js
@@ -12,10 +12,32 @@ import {
   setEditing,
   clearCard,
   setError
-} from '../actions/auth';
+} from '../actions/card';
 
 describe('authReducer', () => {
-  const username = 'testuser';
+  const testResults = [{ image1: 'testimage1' }, { image2: 'testimage2' }];
+  const testImage = {
+    image: {
+      full: 'testfull',
+      thumb: 'testthumb',
+      alt: 'test alt',
+      credit: 'fake photographer',
+      portfolio: 'www.exampleportfolio.com'
+    }
+  };
+  const testMessage = 'this is a test';
+  const testRecipient1 = 'www.example@email.com';
+  const testRecipient2 = 'www.example2@email.com';
+  const index = 1;
+  const testUserCards = [{ card1: {} }, { card2: {} }, { card3: {} }];
+  const testCard = {
+    testCardId: 'asdfi4n2f9v9rHsd92K',
+    image: {},
+    message: testMessage,
+    recipients: [testRecipient1, testRecipient2]
+  };
+  const editing = true;
+  const testError = 'test error';
 
   it('should set the initial state', () => {
     const state = cardReducer(undefined, { type: '__UNKNOWN' });
@@ -39,6 +61,131 @@ describe('authReducer', () => {
       userCards: [],
       editing: false,
       error: null
+    });
+  });
+
+  describe('setResults', () => {
+    let state;
+    state = cardReducer(state, setResults(testResults));
+    expect(state.results).toEqual(testResults);
+  });
+
+  describe('setImage', () => {
+    let state;
+    state = cardReducer(state, setImage(testImage));
+    expect(state.image).toEqual(testImage);
+  });
+
+  describe('flipCard', () => {
+    it('should reverse the state of isCardFlipped', () => {
+      let state;
+      state = cardReducer(state, flipCard());
+      expect(state.isCardFlipped).toEqual(true);
+    });
+  });
+
+  describe('addMessage', () => {
+    it('should update the state with the message', () => {
+      let state;
+      state = cardReducer(state, addMessage(testMessage));
+      expect(state.message).toEqual(testMessage);
+    });
+  });
+
+  describe('addRecipient', () => {
+    it('should update the state with the message', () => {
+      let state;
+      state = cardReducer(state, addRecipient(testRecipient1));
+      expect(state.recipients).toEqual([testRecipient1]);
+    });
+  });
+
+  describe('clearRecipients', () => {
+    it('should update recipients state to empty array', () => {
+      let state;
+      state = cardReducer(state, addRecipient(testRecipient1));
+      state = cardReducer(state, addRecipient(testRecipient2));
+      state = cardReducer(state, clearRecipients());
+      expect(state.recipients).toEqual([]);
+    });
+  });
+
+  describe('deleteEmail', () => {
+    it('should delete the correct email', () => {
+      let state;
+      state = cardReducer(state, addRecipient(testRecipient1));
+      state = cardReducer(state, addRecipient(testRecipient2));
+      state = cardReducer(state, deleteEmail(index));
+      expect(state.recipients).toEqual([testRecipient1]);
+    });
+  });
+
+  describe('displayCards', () => {
+    it('should add userCards to state', () => {
+      let state;
+      state = cardReducer(state, displayCards(testUserCards));
+      expect(state.userCards).toEqual(testUserCards);
+    });
+  });
+
+  describe('setCard', () => {
+    it('should state with currentCard', () => {
+      let state;
+      state = cardReducer(state, setCard(testCard));
+      expect(state).toEqual({
+        results: [],
+        cardId: testCard._testCardId,
+        id: null,
+        image: {},
+        message: testMessage,
+        recipients: [testRecipient1, testRecipient2],
+        isCardFlipped: false,
+        userCards: [],
+        editing: false,
+        error: null
+      });
+    });
+  });
+
+  describe('setEditing', () => {
+    it('should change state to editing: true', () => {
+      let state;
+      state = cardReducer(state, setEditing(editing));
+      expect(state.editing).toEqual(true);
+    });
+  });
+
+  describe('clearCard', () => {
+    it('should set state card keys to null', () => {
+      let state;
+      state = cardReducer(state, setCard(testCard));
+      state = cardReducer(state, clearCard());
+      expect(state).toEqual({
+        results: [],
+        cardId: null,
+        id: null,
+        image: {
+          full: null,
+          thumb: null,
+          alt: null,
+          credit: null,
+          portfolio: null
+        },
+        message: null,
+        recipients: [],
+        isCardFlipped: false,
+        userCards: [],
+        editing: false,
+        error: null
+      });
+    });
+  });
+
+  describe('setError', () => {
+    it('should update state with the error', () => {
+      let state;
+      state = cardReducer(state, setError(testError));
+      expect(state.error).toEqual(testError);
     });
   });
 });
